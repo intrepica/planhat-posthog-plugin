e796477bb9c763bed26f88fdcb2e987d159fb3cd
Terence Munro <terry.munro@literacyplanet.com>
2022-02-09 20:50:40 +1000

    Simplify further because something is still wrong

--- index.js
+++ index.js
@@ -7,24 +7,69 @@
 //  █████        █████░░████████ ████ █████ ████ █████░░████████  ░░█████
 // ░░░░░        ░░░░░  ░░░░░░░░ ░░░░ ░░░░░ ░░░░ ░░░░░  ░░░░░░░░    ░░░░░
 
-export async function setupPlugin({ config, attachments, global, metrics }) {
+
+// TODO: Customizable Mapping of event -> action
+function toPlanhat(event) {
+  const action = event.event.toLowerCase().replace(/[^a-z]/g, "");
+  const current_url =
+    event.properties?.$pathname?.includes("auth") ? {}
+      : { current_url: event.properties?.$current_url };
+  return {
+    action,
+    externalId: event.properties?.$user_id ?? event.distinct_id,
+    info: {
+      ...current_url,
+      host: event.properties?.$host,
+      pathname: event.properties?.$pathname,
+      os: event.properties?.$os ?? event.$set?.$os,
+      browser: event.properties?.$browser ?? event.$set?.$browser,
+      browser_version:
+        event.properties?.$browser_version ?? event.$set?.$browser_version,
+      screen_height: event.properties?.$screen_height,
+      screen_width: event.properties?.$screen_width,
+      viewport_height: event.properties?.$viewport_height,
+      viewport_width: event.properties?.$viewport_width,
+      initial_referrer:
+        event.properties?.$initial_referrer ?? event.$set?.$initial_referrer,
+      initial_referring_domain:
+        event.properties?.$initial_referring_domain ??
+        event.$set?.$initial_referring_domain,
+      alias: event.properties?.alias,
+      year: event.properties?.year,
+      month: event.properties?.month,
+      day: event.properties?.day,
+      day_of_the_week: event.properties?.day_of_the_week,
+    },
+  };
+}
+
+export function setupPlugin({ config, attachments, global, metrics }) {
   metrics.pluginLoads.increment(1);
 
-  global.eventWhitelist = new Set(config.eventWhitelist ? config.eventWhitelist.split(",") : []);
-  console.info(`${global.eventWhitelist.size} events whitelisted`);
+  if (config.eventWhitelist) {
+    global.eventWhitelist = new Set(config.eventWhitelist.split(","));
+    console.info(`${global.eventWhitelist.size} events whitelisted`);
+  }
 
-  global.eventBlacklist = new Set(config.eventBlacklist ? config.eventBlacklist.split(",") : []);
-  console.info(`${global.eventBlacklist.size} events blacklisted`);
+  if (config.eventBlacklist) {
+    global.eventBlacklist = new Set(config.eventBlacklist.split(","));
+    console.info(`${global.eventBlacklist.size} events blacklisted`);
+  }
 
   if (attachments.userWhitelist) {
-    const attachedWhitelist = Buffer.from(attachments.userWhitelist.contents)
-      .toString()
-      .split("\n");
-    if (attachedWhitelist.length > 0) {
-      if (attachedWhitelist[0] === "externalId") {
-        attachedWhitelist.shift();
+    try {
+      const attachedWhitelist = Buffer.from(attachments.userWhitelist.contents)
+        .toString()
+        .split("\n");
+      if (attachedWhitelist.length > 0) {
+        if (attachedWhitelist[0] === "externalId") {
+          attachedWhitelist.shift();
+        }
+        global.userWhitelist = new Set(attachedWhitelist);
+        console.info(`${global.userWhitelist.size} users whitelisted`);
       }
-      global.userWhitelist = new Set(attachedWhitelist);
+    } catch (e) {
+      console.error("Creation of User Whitelist Failed:", e);
     }
   }
 
@@ -43,18 +88,16 @@ export async function setupPlugin({ config, attachments, global, metrics }) {
     } catch (e) {
       console.error("Creation of User Blacklist Failed:", e);
     }
-  } else {
-    global.userBlacklist = new Set();
   }
 
   // TODO: Support whitelisting by Cohort
 }
 
-export const jobs = {
-  flushBatch: async () => {
+// export const jobs = {
+//   flushBatch: async () => {
 
-  }
-};
+//   }
+// };
 
 export async function teardownPlugin({ global }) {
   if (global.buffer?.flush) {
@@ -136,41 +179,6 @@ export async function exportEvents(events, { cache, config, global, metrics }) {
       console.error("Error sending event to Planhat:", e);
     }
   }
-}
-
-// TODO: Customizable Mapping of event -> action
-export function toPlanhat(event) {
-  const action = event.event.toLowerCase().replace(/[^a-z]/g, "");
-  const current_url =
-    event.properties?.$pathname?.includes("auth") ? {}
-      : { current_url: event.properties?.$current_url };
-  return {
-    action,
-    externalId: event.properties?.$user_id ?? event.distinct_id,
-    info: {
-      ...current_url,
-      host: event.properties?.$host,
-      pathname: event.properties?.$pathname,
-      os: event.properties?.$os ?? event.$set?.$os,
-      browser: event.properties?.$browser ?? event.$set?.$browser,
-      browser_version:
-        event.properties?.$browser_version ?? event.$set?.$browser_version,
-      screen_height: event.properties?.$screen_height,
-      screen_width: event.properties?.$screen_width,
-      viewport_height: event.properties?.$viewport_height,
-      viewport_width: event.properties?.$viewport_width,
-      initial_referrer:
-        event.properties?.$initial_referrer ?? event.$set?.$initial_referrer,
-      initial_referring_domain:
-        event.properties?.$initial_referring_domain ??
-        event.$set?.$initial_referring_domain,
-      alias: event.properties?.alias,
-      year: event.properties?.year,
-      month: event.properties?.month,
-      day: event.properties?.day,
-      day_of_the_week: event.properties?.day_of_the_week,
-    },
-  };
 }
 
 export const metrics = {
